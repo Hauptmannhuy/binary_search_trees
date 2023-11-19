dd4d8f3df64401a2a4a874f1d54d7b8991c7e633
Hauptmannhuy <dindago2324@gmail.com>
2023-11-19 20:12:26 +0200

    implemented delete method and level_order

--- binary_search_tree.rb
+++ binary_search_tree.rb
@@ -14,7 +14,7 @@ class Tree
     @arr = Array.new(10){rand(100)}.uniq.sort
     @root = nil
   end
-
+  
   def build_tree(arr,start,end_arr)
     return nil if start > end_arr
     mid = (end_arr+start)/2
@@ -24,6 +24,30 @@ class Tree
     @root = root
     return root
 end 
+
+def find(val)
+  current = @root
+  while current
+    return current if val == current.data
+    current.data > val ? current = current.left : current = current.right
+  end
+  nil
+end
+
+def level_order
+  queue = [@root] 
+  print_out = []
+  while !queue.empty?
+    queue << queue[0].left if !queue[0].left.nil?
+    queue << queue[0].right if !queue[0].right.nil?
+    print_out << queue.shift
+  end
+  if block_given?
+  print_out.each{|node| yield node}
+else
+  print_out
+ end
+end
 
 def insert(val=0)
   current_root = self.root
@@ -60,30 +84,51 @@ def delete(val)
   current = @root
   previous = @root
   direction = false
+  inorder = nil
 
   while current
     if current.data == val
       if !current.left && !current.right
         direction == false ? previous.left = nil : previous.right = nil
-        return
-      end
+      elsif (!current.left && current.right) || (current.left && !current.right)
+        direction == false ? previous.left = current.left : previous.right = current.right
+      elsif current.left && current.right
+
+        previous_of_current = current
+        1.times{current = current.right}
+     if !current.left.nil?
+        inorder = current.left
+        inorder.left = previous_of_current.left
+        current.left = nil
+        inorder.right = current.right
+        direction == false ? previous.left = inorder : previous.right = inorder
       else
-        if current.data > val
-          previous = current
-          current.left.nil? ? current = current.right : current = current.left
-          direction = false
-        else
-          previous = current
-          current = current.right.nil? ? current = current.left : current = current.right
-          direction = true
-        end
-  end
+        inorder = previous_of_current.right
+        inorder.left = previous_of_current.left
+        direction == false ? previous.left = inorder : previous.right = inorder
+      end
+      
+    end
+      return
+    else
+      if current.data > val
+        previous = current
+        current.left.nil? ? current = current.right : current = current.left
+        direction = false
+      else
+        previous = current
+        current = current.right.nil? ? current = current.left : current = current.right
+        direction = true
+      end
+    end
+    
 end
 end
 #  check if the current.data == val if false then follow next steps 1; else follow steps 2
 #  1. then compare data and val, traverse deeply due to result and check again
 #  2. check left and right child. if there is no child then follow step 2.1
 #  2.1 set reference of previous node to current node
+#  2.3 
 
 def traverse
   current = @root
@@ -97,9 +142,4 @@ end
 end
 tree = Tree.new()
 n = tree.arr.length
-# tree.build_tree(tree.arr,0,n-1)
-
-tree.insert(1)
-tree.insert(2)
-tree.insert(3)
-tree.insert(4)
+tree.build_tree(tree.arr,0,n-1)
